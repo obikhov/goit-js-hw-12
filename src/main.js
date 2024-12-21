c7620a5e4ebf7a0eee301d6fb36b8866f85346dd
Vladislav <160542917+obikhov@users.noreply.github.com>
2024-12-21 13:23:50 +0200

    f
    f

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchImages } from './pixabay-api.js';
-import { createImageMarkup, updateGallery, clearGallery } from './render-functions.js';
+import { fetchImages } from './js/pixabay-api.js';
+import { createImageMarkup, updateGallery, clearGallery } from './js/render-functions.js';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('#gallery');
